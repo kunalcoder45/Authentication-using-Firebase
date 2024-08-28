5b97bf69c3d9cdca473a3dc8a5abe52b2cc449ae
kunalcoder45 <159230510+kunalcoder45@users.noreply.github.com>
2024-08-29 00:48:02 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getAuth, signInWithPopup, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAhr2QrGgAZ4CiX1E_NGuCTEjO5mdO8rR0",
@@ -38,30 +38,4 @@ signInWithEmailAndPassword(auth, email, password)
     });
 });
 
-
-
-/* Google Authentication*/
-
-
-const provider = new GoogleAuthProvider();
-
-const googleLogin = document.getElementById("google-login-btn");
-
-googleLogin.addEventListener("click", function() {
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const user = result.user;
-            console.log(user);
-            // Redirect after successful login
-            window.location.href = "/logged.html";
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error("Error during sign-in:", errorCode, errorMessage);
-            // Optionally, display the error to the user
-            alert(`Sign-in failed: ${errorMessage}`);
-        });
-});
 
