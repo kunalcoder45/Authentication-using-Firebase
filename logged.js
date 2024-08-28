b122725f9ccbc9134f1e02fedfb71d26b12a8997
kunalcoder45 <159230510+kunalcoder45@users.noreply.github.com>
2024-08-29 00:13:57 +0530

    Update logged.js

--- logged.js
+++ logged.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, singOut } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, onAuthStateChanged , signOut } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAhr2QrGgAZ4CiX1E_NGuCTEjO5mdO8rR0",
@@ -10,11 +9,15 @@ const firebaseConfig = {
   messagingSenderId: "483187118922",
   appId: "1:483187118922:web:671077478349e12edf67b3",
   measurementId: "G-7Y7Z6GGWZ4"
-};
+
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const database = getDatabase (app);
+// auth.languageCode = "en"
+// const provider = new GoogleAuthProvider();
+
+const user = auth.currentUser;
 
 function updateUserProfile(user) {
   const userName = user.displayName || "No name provided";
@@ -26,17 +29,42 @@ function updateUserProfile(user) {
   document.getElementById("userProfilePicture").src = userProfilePicture;
 }
 
+// This function checks the authentication state
 onAuthStateChanged(auth, (user) => {
   if (user) {
+    // If user is signed in, update profile UI
     updateUserProfile(user);
+    //console.log("User ID:", user.uid);
     const uid = user.uid;
-    console.log("User ID:", uid);
     return uid;
+    
   } else {
+    // If no user is signed in, redirect to login page
     alert("Please create an account & log in.");
     window.location.href = "/index.html";
   }
 });
+
+// Ensure that the auth state is being checked after DOM content is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      updateUserProfile(user);
+    }
+  });
+});
+
+// log out
+const logOut = document.getElementById("logout-btn");
+
+logOut.addEventListener("click", function() {
+signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
+});
+
 
 
 // Sign out function
@@ -50,3 +78,5 @@ document.getElementById('logout-btn').addEventListener('click', function() {
         console.error('Sign out error:', error);
     });
 });
+
+
