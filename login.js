b258b0589426fb1a46396d9409330a5e4721e1ca
kunalcoder45 <159230510+kunalcoder45@users.noreply.github.com>
2024-08-28 23:07:52 +0530

    Update login.js

--- login.js
+++ login.js
@@ -18,7 +18,6 @@ auth.languageCode = 'en';
 const database = getDatabase(app);
 
 document.getElementById('login-btn').addEventListener('click', function() {
-    const username = document.getElementById('login-username').value;
     const email = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
 
