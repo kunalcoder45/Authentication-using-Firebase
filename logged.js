c501535b72fc2994833f72b8f9c217e82a5cc819
kunalcoder45 <159230510+kunalcoder45@users.noreply.github.com>
2024-08-28 22:06:36 +0530

    Update logged.js

--- logged.js
+++ logged.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/fireba
 import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLRgwJCXDz9-NtntVCJDKB5tEPO5WwzAk",
-  authDomain: "auth-c20e3.firebaseapp.com",
-  projectId: "auth-c20e3",
-  storageBucket: "auth-c20e3.appspot.com",
-  messagingSenderId: "37692570985",
-  appId: "1:37692570985:web:1f6369f1f3456f6acb91f8",
-  measurementId: "G-XKW1J4TP3Y"
+  apiKey: "AIzaSyAhr2QrGgAZ4CiX1E_NGuCTEjO5mdO8rR0",
+  authDomain: "user-authentication-47380.firebaseapp.com",
+  projectId: "user-authentication-47380",
+  storageBucket: "user-authentication-47380.appspot.com",
+  messagingSenderId: "483187118922",
+  appId: "1:483187118922:web:671077478349e12edf67b3",
+  measurementId: "G-7Y7Z6GGWZ4"
 };
 
 const app = initializeApp(firebaseConfig);
