a6243baa98a993ea9170b279c49bbf7589cc1aa6
kunalcoder45 <159230510+kunalcoder45@users.noreply.github.com>
2024-08-28 22:51:06 +0530

    Update login.js

---
+++ login.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhr2QrGgAZ4CiX1E_NGuCTEjO5mdO8rR0",
+  authDomain: "user-authentication-47380.firebaseapp.com",
+  projectId: "user-authentication-47380",
+  storageBucket: "user-authentication-47380.appspot.com",
+  messagingSenderId: "483187118922",
+  appId: "1:483187118922:web:671077478349e12edf67b3",
+  measurementId: "G-7Y7Z6GGWZ4"
+};
+
+signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        console.log('Logged in:', user);
+        alert('Log In Successful!');
+
+        // Redirect to another page after successful login
+        window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error('Log In Error:', errorCode, errorMessage);
+        alert('Error during Log In: ' + errorMessage);
+    });
