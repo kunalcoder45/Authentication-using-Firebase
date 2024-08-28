e731417b0855ed2b2dfc698cbcde5072a00455e0
kunalcoder45 <159230510+kunalcoder45@users.noreply.github.com>
2024-08-29 00:44:54 +0530

    Update

---
+++ google-auth.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhr2QrGgAZ4CiX1E_NGuCTEjO5mdO8rR0",
+  authDomain: "user-authentication-47380.firebaseapp.com",
+  projectId: "user-authentication-47380",
+  storageBucket: "user-authentication-47380.appspot.com",
+  messagingSenderId: "483187118922",
+  appId: "1:483187118922:web:671077478349e12edf67b3",
+  measurementId: "G-7Y7Z6GGWZ4"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+auth.languageCode = 'en';
+const database = getDatabase(app);
+
+
