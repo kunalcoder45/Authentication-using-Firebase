c724f4388dd5d38e0fe97419adfbc2169720c314
kunalcoder45 <159230510+kunalcoder45@users.noreply.github.com>
2024-08-31 12:06:46 +0530

    Update

---
+++ generate-registration-options.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require 'vendor/autoload.php';
+
+use lbuchs\WebAuthn\WebAuthn;
+
+$webAuthn = new WebAuthn('Your Website', 'https://your-website.com');
+
+// Fetch the user's UID from the POST request
+$data = json_decode(file_get_contents('php://input'), true);
+$uid = $data['uid'];
+
+// Generate the WebAuthn registration options
+$options = $webAuthn->getCreateArgs($uid);
+
+// Store the challenge in the session
+$_SESSION['challenge'] = $webAuthn->getChallenge();
+
+// Return the options as a JSON response
+header('Content-Type: application/json');
+echo json_encode($options);
+?>
